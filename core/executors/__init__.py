4eb49fe2dbcf24148b88a230467a99eeb4b0a5f4
yunxzh <116542849+yunxzh@users.noreply.github.com>
2025-11-26 00:30:34 -0500

    Update __init__.py

--- core/executors/__init__.py
+++ core/executors/__init__.py
@@ -1,7 +1,7 @@
 """
 执行器模块
 """
-from .signin_executor import SignInExecutor
-from .reply_executor import ReplyExecutor
+from core.executors.signin_executor import SignInExecutor
+from core.executors.reply_executor import ReplyExecutor
 
 __all__ = ['SignInExecutor', 'ReplyExecutor']
